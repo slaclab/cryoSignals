0949c651dc42d99bc498bed591210c7802e161d2
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-08-08 17:51:16 -0700

    adding readbacks

--- cryo_signals.py
+++ cryo_signals.py
@@ -2,11 +2,11 @@ from typing import Dict, List
 
 import numpy as np
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QGridLayout, QWidget
+from PyQt5.QtWidgets import QFormLayout, QGridLayout, QHBoxLayout, QWidget
 from lcls_tools.superconducting.scLinac import (CRYOMODULE_OBJECTS, Cryomodule,
                                                 L0B, L1B, L1BHL, L2B, L3B)
 from pydm import Display
-from pydm.widgets import PyDMTimePlot
+from pydm.widgets import PyDMLabel, PyDMTimePlot
 
 
 def get_dimensions(options: List[str]):
@@ -18,6 +18,19 @@ def get_dimensions(options: List[str]):
     return col_count
 
 
+class CryoReadbacks():
+    def __init__(self, cryomodule: Cryomodule):
+        super().__init__()
+        self.main_layout: QFormLayout = QFormLayout()
+        
+        for pv in [cryomodule.dsLevelPV, cryomodule.usLevelPV,
+                   cryomodule.pvPrefix + "AACTMEANSUM",
+                   cryomodule.jtValveReadbackPV]:
+            label = PyDMLabel(pv)
+            label.alarmSensitiveContent = True
+            self.main_layout.addRow(pv, label)
+
+
 class CryoPlot(PyDMTimePlot):
     def __init__(self, cryomodule: Cryomodule):
         super().__init__()
@@ -26,6 +39,8 @@ class CryoPlot(PyDMTimePlot):
         self.setPlotTitle(f"CM{cryomodule.name}")
         self.setUpdatesAsynchronously(True)
         self.setAutoRangeY(False)
+        self.setShowYGrid(True)
+        self.setShowXGrid(True)
         
         self.ds_axis = self.addAxis(plot_data_item=None, name="DS Level",
                                     orientation="left",
@@ -78,8 +93,13 @@ class CryoSignals(Display):
             col_count = get_dimensions(cryo_list)
             
             for cryo_idx, cryo_name in enumerate(cryo_list):
-                cryo_plot = CryoPlot(CRYOMODULE_OBJECTS[cryo_name])
-                grid_layout.addWidget(cryo_plot, int(cryo_idx / col_count),
+                cm_layout = QHBoxLayout()
+                cryomodule = CRYOMODULE_OBJECTS[cryo_name]
+                cryo_plot = CryoPlot(cryomodule)
+                readbacks = CryoReadbacks(cryomodule).main_layout
+                cm_layout.addWidget(cryo_plot)
+                cm_layout.addLayout(readbacks)
+                grid_layout.addLayout(cm_layout, int(cryo_idx / col_count),
                                       cryo_idx % col_count)
                 self.plots[cryo_name] = cryo_plot
         
